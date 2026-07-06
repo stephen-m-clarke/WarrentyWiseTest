dbe2db130c17ba786ede3f386b2e1e0ac0d678e1
stephen-m-clarke <stephen.clarke.17497@gmail.com>
2026-07-06 17:44:44 +0100

    feature: reduced page content horizontal padding

--- store/src/components/AppHeader/AppHeader.tsx
+++ store/src/components/AppHeader/AppHeader.tsx
@@ -5,8 +5,10 @@ import { CONTENT_MAX_WIDTH } from "../../variables";
 export const AppHeader: React.FC = () => {
   return (
     <Header>
-      <Box maw={CONTENT_MAX_WIDTH} mx="auto" w="100%" px={64}>
-        <Text size="lg" fw={700} c="white">Store App</Text>
+      <Box maw={CONTENT_MAX_WIDTH} mx="auto" w="100%" px={32}>
+        <Text size="lg" fw={700} c="white">
+          Store App
+        </Text>
       </Box>
     </Header>
   );
