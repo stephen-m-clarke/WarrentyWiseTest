6cdbc5f15188717e6ace19db64b48c4ddc0d0db7
stephen-m-clarke <stephen.clarke.17497@gmail.com>
2026-07-07 13:58:47 +0100

    chore: cleaned up unused files and moved duplicated config to a shared folder

--- component-library-package/vitest.config.ts
+++ component-library-package/vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./vitest.setup.js'],
+    setupFiles: ['./../config/vitest.setup.js'],
   },
 });
